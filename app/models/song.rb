35613d1b3e46a98e622c2d37000a982184e31bf8
Simon Mei <smei8800@gmail.com>
2019-03-04 10:56:45 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,16 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    drake = Artist.find_by(name: "Drake")
+    if drake.nil?
+      drake = Artist.create(name: "Drake")
+    else
+      self.artist = drake
+    end
   end
 end
